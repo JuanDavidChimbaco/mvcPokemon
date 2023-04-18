eda51453ed202a096c36eddfb9cb788a159a3fec
Juan David Ch <dajun318@gmail.com>
2023-04-18 11:11:47 -0500

    vistas añadidas

---
+++ view/roles.frm.php
@@ -0,0 +1,11 @@
+<?php 
+include_once "header.php";
+// body
+?>
+    <div class="row">
+        <h2 class="text-center bg-dark text-white">Roles</h2>
+    </div>
+    div
+<?php
+// body
+include_once "footer.php";
