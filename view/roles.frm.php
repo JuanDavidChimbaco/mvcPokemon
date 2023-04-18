689ef295088105f5dc61825d5a331f6bb5d7a80a
Juan David Ch <dajun318@gmail.com>
2023-04-18 11:13:53 -0500

    "Update 3"

--- view/roles.frm.php
+++ view/roles.frm.php
@@ -5,7 +5,7 @@ include_once "header.php";
     <div class="row">
         <h2 class="text-center bg-dark text-white">Roles</h2>
     </div>
-    div
+    
 <?php
 // body
 include_once "footer.php";
