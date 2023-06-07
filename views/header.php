a4dc194e39fb4ea75398657c2c83aff3283db7fc
Juan David Chimbaco <dajun318@gmail.com>
2023-06-07 18:52:09 -0500

    Mejora del diseño del login

--- views/header.php
+++ views/header.php
@@ -5,23 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
+
     <!-- Sweet Alert 2 -->
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    
+
     <!-- Bootstrap 5.3 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../views/assets/css/bootstrap.min.css">
     <script src="../views/assets/js/bootstrap.min.js"></script>
-    
+
     <!-- jquery Local -->
     <script src="assets/js/jquery-3.7.0.js"></script>
 
     <!-- dataTable -->
-    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet"/>
+    <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet" />
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-    
+
     <!-- DataTable Buttons -->
     <script src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
@@ -29,17 +32,17 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
-    
-    <link href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" rel="stylesheet"/>
-    
+
+    <link href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" rel="stylesheet" />
+
 
     <!-- Font Awaysome Local-->
     <script src="assets/js/all.js"></script>
     <link rel="stylesheet" href="assets/css/all.css">
-    
+
     <!-- Styles Local -->
     <link rel="stylesheet" href="assets/css/app.css">
-    
+
     <title>Soul Knigth</title>
 
     <!--  icono en la pestalla del navegador-->
@@ -56,8 +59,10 @@
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">HOME</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand active" href="#">HOME</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="true" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -68,21 +73,8 @@
                         <li class="nav-item">
                             <a class="nav-link" href="../views/productosForm.php">PRODUCTOS</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link" href="../views/productosForm.php" >USUARIOS</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
--- views/login.php
+++ views/login.php
@@ -6,44 +6,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- boostrap 5.3  -->
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <script src="./assets/js/bootstrap.min.js"></script>
 
+    <!-- Hoja de estilos local -->
+    <link rel="stylesheet" href="assets/css/stylesLogin.css">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,600;1,100&family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
     <title>Log In</title>
+
 </head>
 
 <body>
-    <div class="container">
-        <div class="row text-center pt-5">
-            <h1>LOG IN</h1>
-        </div>
-        <div class="row d-grid justify-content-center pt-5">
-            <div class="col">
-                <label for="" class="form-label">Username - Email</label>
-                <input type="email" name="txtCorreo" id="txtCorreo" class="form-control">
-            </div>
-            <div class="col">
-                <label for="" class="form-label">Password</label>
-                <input type="password" name="txtPassword" id="txtPassword" class="form-control">
-            </div>
-            <div class="col mt-4 text-center pb-5">
-                <button type="button" class="btn btn-primary" onclick="login()">Sign In</button>
-            </div>
-        </div>
-        <div class="row text-center d-grid pt-4">
-            <div class="col">
-                <p class="form-text">
-                    No tienes Cuenta ?
-                </p>
-            </div>
-            <div class="col">
-                <a href="" class="btn btn-info"> Registrese Aqui </a>
+    <div class="background-container">
+        <div class="card-container">
+            <div class="card">
+                <h1 class="text-center titulo">LOG IN</h1>
+                <div class="mb-3">
+                    <label for="txtCorreo" class="form-label">Username - Email</label>
+                    <input type="email" name="txtCorreo" id="txtCorreo" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="txtPassword" class="form-label">Password</label>
+                    <input type="password" name="txtPassword" id="txtPassword" class="form-control">
+                </div>
+                <div class="text-center">
+                    <button type="button" class="btn btn-primary" onclick="login()">Sign In</button>
+                </div>
             </div>
         </div>
-        </dvi>
-        <script src="./assets/js/login.js"></script>
+    </div>
+    <script src="./assets/js/login.js"></script>
 </body>
 
 </html>
